69c8674415889c6c32248c2e5c2badfb56eb0007
nequals30 <nequals30@gmail.com>
2023-04-08 12:13:18 -0500

    local javascript first pass

---
+++ rsvp.js
@@ -0,0 +1,19 @@
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = this.elements.name.value;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://script.google.com/macros/s/YOUR_GOOGLE_APPS_SCRIPT_ID/exec?name=' + name, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        var idAndNames = xhr.responseText;
+        if (idAndNames === "Name not found") {
+          document.getElementById('output').innerHTML = "<div>Error: Couldn't find guest name.</div>";
+        } else if (idAndNames === "Already RSVPd"){
+          document.getElementById('output').innerHTML = `<div>Error: Already RSVP'd.</div>`;
+        } else {
+            document.getElementById('output').innerHTML = "<div>Name found!</div>";
+        }
+      }
+    };
+    xhr.send();
+  });
